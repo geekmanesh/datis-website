d54c48019d1ad60de5d1d903d48fbc41469e2621
Hamid <dimah.code@gmail.com>
2026-05-23 16:53:29 +0330

    feat: add users translation for farsi in control panel

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
-from .models import User
 from unfold.admin import ModelAdmin
+from .models import User
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group
 
 
 @admin.register(User)
-class UserAdmin(ModelAdmin):
-    list_display = ["email", "full_name"]
+class ProjectAdmin(ModelAdmin):
+    list_display = ["full_name", "email", "is_superuser"]
+    exclude = ["id", "last_login"]
+    fieldsets = (
+        (
+            _("Basic Information"),
+            {
+                "fields": (
+                    "full_name",
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {"fields": ("user_permissions",)},
+        ),
+        (
+            _("Status"),
+            {
+                "fields": (
+                    "is_superuser",
+                    "is_staff",
+                    "is_active",
+                )
+            },
+        ),
+    )
+
+
+admin.site.unregister(Group)
--- users/models.py
+++ users/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import UserManager
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    full_name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    email = models.EmailField(unique=True, verbose_name=_("Email"))
+    full_name = models.CharField(max_length=255, verbose_name=_("Full Name"))
+    is_active = models.BooleanField(default=True, verbose_name=_("Active"))
+    is_staff = models.BooleanField(default=False, verbose_name=_("Staff"))
 
     objects = UserManager()
 
@@ -16,3 +17,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.full_name
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
