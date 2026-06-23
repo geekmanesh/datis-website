d21836bc87c5ef25242f3fd0253db0db67b6ddb2
Hamid <geekmanesh@gmail.com>
2026-06-23 15:53:09 +0330

    feat: implement service detail class and register url, closes #18

--- services/urls.py
+++ services/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ServicesList
+from .views import ServicesList, ServiceDetial
 
 urlpatterns = [
     path("", ServicesList.as_view(), name="services"),
+    path("<slug:slug>/", ServiceDetial.as_view(), name="service-detail"),
 ]
--- services/views.py
+++ services/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class ServicesList(TemplateView):
     template_name = "pages/services.html"
+
+
+class ServiceDetial(TemplateView):
+    template_name = "pages/services-details.html"
