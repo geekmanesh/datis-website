e2a4ad7733bae01e229c9b50d0812a17b9b0147c
Hamid <geekmanesh@gmail.com>
2026-06-23 15:52:38 +0330

    feat: implement project detail template class and register url, closes #16

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProjectsList
+from .views import ProjectsList, ProjectDetail
 
 urlpatterns = [
     path("", ProjectsList.as_view(), name="projects"),
+    path("<slug:slug>/", ProjectDetail.as_view(), name="project-detail"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class ProjectsList(TemplateView):
     template_name = "pages/projects.html"
+
+
+class ProjectDetail(TemplateView):
+    template_name = "pages/projects-details.html"
