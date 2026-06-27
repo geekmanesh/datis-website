c906cba27aa5212f4adcc7c102a2292bcf0b9f65
Hamid <geekmanesh@gmail.com>
2026-06-27 22:16:45 +0330

    refactor: restructure services template folder to services/list.html, detail.html, closed #55

--- services/views.py
+++ services/views.py
@@ -5,13 +5,13 @@ from .models import Service
 
 class ServicesListView(ListView):
     model = Service
-    template_name = "pages/services.html"
+    template_name = "services/list.html"
     context_object_name = "services"
 
 
 class ServiceDetailView(DetailView):
     model = Service
-    template_name = "pages/services-details.html"
+    template_name = "services/detail.html"
     context_object_name = "service"
     slug_field = "slug"
     slug_url_kwarg = "slug"
