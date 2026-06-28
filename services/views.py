d4cd017a3aa36c03997681c3b913aef2d838e0fb
Hamid <geekmanesh@gmail.com>
2026-06-28 13:20:12 +0330

    feat: Implement services sub-section in service detail, closed #47

--- services/views.py
+++ services/views.py
@@ -15,3 +15,8 @@ class ServiceDetailView(DetailView):
     context_object_name = "service"
     slug_field = "slug"
     slug_url_kwarg = "slug"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["services"] = Service.objects.all()[:10]
+        return context
