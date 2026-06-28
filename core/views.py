0c9d3894d90d47335db0fc8601607d1d7d08b3c2
Hamid <geekmanesh@gmail.com>
2026-06-28 15:13:28 +0330

    feat: Implement projects section in homepage, closed #46

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 
+from projects.models import Project
 from services.models import Service
 
 
@@ -9,6 +10,7 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["services"] = Service.objects.all()[:10]
+        context["projects"] = Project.objects.prefetch_related("images")[:5]
         return context
 
 
