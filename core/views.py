0f83df8a3af17e0dcb169221b682af4288ed3d04
Hamid <geekmanesh@gmail.com>
2026-06-28 14:29:16 +0330

    feat: Implement services sub-section in homepage, closed #45

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django.views.generic import TemplateView
 
+from services.models import Service
+
 
 class HomePageView(TemplateView):
     template_name = "home/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["services"] = Service.objects.all()[:10]
+        return context
 
 
 class AboutPageView(TemplateView):
